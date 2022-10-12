a275c1a328a60eb2af65338e6dcdf5d04c4c7033
lil010 <lil010@ucsd.edu>
2022-10-12 11:43:22 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+class Handler implements URLHandler{
+
+    ArrayList<String> searchList = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        
+    }
+}
